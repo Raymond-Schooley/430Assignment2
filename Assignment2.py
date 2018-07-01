e4400bdad16bd05891e0a1a8ab2e5da018f6f5a6
timonyang@gmail.com <timonyang@gmail.com>
2018-06-30 23:18:38 -0700

    start readme, format code

--- Assignment2.py
+++ Assignment2.py
@@ -4,16 +4,26 @@ import netifaces
 google_ip = '2607:f8b0:400a:809::200e'
 
 #this is import for retrieving the subnet mask
-interface = '{46DE3F82-8FB5-469E-8D06-EC97A8378082}'
+#interface = '{46DE3F82-8FB5-469E-8D06-EC97A8378082}'
 
 #get this computer's ipv4 and ipv6 address
 ipv4 = socket.getaddrinfo(socket.gethostname(), 0)[3][4][0]
 ipv6 = socket.getaddrinfo(socket.gethostname(), 0)[2][4][0]
 print(ipv4)
-print(ipv6)
+print(ipv6, "\n")
 
 #get the subnet mask
-print("Mask: ", netifaces.ifaddresses(interface)[netifaces.AF_INET][0]['netmask'])
+#print("Mask: ", netifaces.ifaddresses(interface)[netifaces.AF_INET][0]['netmask'])
+
+for i in netifaces.interfaces():
+    try:
+        # Address
+        print("IP Address: ", netifaces.ifaddresses(i)[netifaces.AF_INET][0]['addr'])
+        print("IP Address: ", netifaces.ifaddresses(i)[netifaces.AF_INET6][0]['addr'])
+        print("Mask: ", netifaces.ifaddresses(i)[netifaces.AF_INET][0]['netmask'])
+        print("Gateway: ", netifaces.gateways()['default'][netifaces.AF_INET][0], "\n")
+        
+    except:pass
 
 #given an ip address get the fqdn
 google_fqdn = socket.getfqdn(socket.gethostbyaddr(google_ip)[2][0])
--- RaymondParse.py
+++ RaymondParse.py
@@ -1,60 +1,53 @@
 import subprocess
-
 import os
-
-
 import re
 
-#program driver
+# program driver
+
+
 def main():
-
-
-
-    
-file = open('networkconfigoutput.txt', 'wb')
+    file = open('networkconfigoutput.txt', 'wb')
     get_network_info(file)
 
-
-    get_ip_fqdn(file)        
-
-
+    get_ip_fqdn(file)
 
     file.close()
 
     #os.startfile("networkconfigoutput.txt", "print")
 
+
 '''runs the ipconfig command and parses it to find the useful info only
 returns a list: result(ipv6, ipv4, subnet mask)'''
 
+
 def get_network_info(file):
-    #Header to identify our target info
+    # Header to identify our target info
     ipv4_id = 'IPv4 Address'
     ipv6_id = 'IPv6 Address'
     subnet_id = 'Sunet Mask '
-    #list to return all info at once
+    # list to return all info at once
     result = list()
-    #Ipconfig will get ipv4, ipv6 and subnet mask
+    # Ipconfig will get ipv4, ipv6 and subnet mask
 
     ipconfig_response = subprocess.Popen(["ipconfig"], stdout=subprocess.PIPE)
 
-
     for line in ipconfig_response.stdout:
 
-        #Apparently this returns something other than string so we need to convert to string
+        # Apparently this returns something other than string so we need to convert to string
         line = line.decode('utf-8')
         line = line.strip()
-        #get rid of weird characters
+        # get rid of weird characters
         line = re.sub("[b']", '', line)
-        #break of the header part
+        # break of the header part
         str = line.split('.', 1)[0]
-        #check header
+        # check header
         if str == ipv4_id or str == ipv6_id or str == subnet_id:
-            #When we find a target save to result list
+            # When we find a target save to result list
             str1 = line.split(':', 1)[1].strip()
             result.append(str1)
-        #file.write(line)
+        # file.write(line)
 
-        #file.write(b'\r\n')
+        # file.write(b'\r\n')
 
     print(result)
     return result
@@ -64,13 +57,14 @@ def get_network_info(file):
 If it an ip address get the fqdn or vice versa
 Finally call the ping and tracert commands with the info'''
 
+
 def get_ip_fqdn(file):
     user = input('Enter a ip address or fqdn')
 
-    #ipv4, ipv6, or fqdn?
+    # ipv4, ipv6, or fqdn?
     type = what_is(user)
 
-    #Find corresponding info, call ping and tracert with appropriate option
+    # Find corresponding info, call ping and tracert with appropriate option
     if type == 1:
         print('You entered a ipv4 address, the fqdn is ' + get_fqdn(user))
         ping(user, '-4', file)
@@ -88,9 +82,10 @@ def get_ip_fqdn(file):
 
 '''We are just running ping and print results to a file'''
 
-def ping(ip, option, file):
-    ping_result = subprocess.Popen(['ping', ip, option], stdout=subprocess.PIPE)
 
+def ping(ip, option, file):
+    ping_result = subprocess.Popen(
+        ['ping', ip, option], stdout=subprocess.PIPE)
 
     for line in ping_result.stdout:
 
@@ -98,17 +93,17 @@ def ping(ip, option, file):
 
         print(line)
 
-        #file.write(line)
+        # file.write(line)
 
-        #file.write(b'\r\n')
-
+        # file.write(b'\r\n')
 
 
 '''We are just running tracert and print results to a file'''
 
-def tracert(ip, option, file):
-    tracert_result = subprocess.Popen(['tracert', option, ip], stdout=subprocess.PIPE)
 
+def tracert(ip, option, file):
+    tracert_result = subprocess.Popen(
+        ['tracert', option, ip], stdout=subprocess.PIPE)
 
     for line in tracert_result.stdout:
 
@@ -116,18 +111,18 @@ def tracert(ip, option, file):
 
         print(line)
 
-        #file.write(line)
+        # file.write(line)
 
-        #file.write(b'\r\n')
-
-
+        # file.write(b'\r\n')
 
 
 '''Given an ip address(ipv4 or ipv6) get the fqdn'''
 
+
 def get_fqdn(ip):
     result = 'Fail'
-    fqdn_nslookup_response = subprocess.Popen(["nslookup", ip], stdout=subprocess.PIPE)
+    fqdn_nslookup_response = subprocess.Popen(
+        ["nslookup", ip], stdout=subprocess.PIPE)
     for line in fqdn_nslookup_response.stdout:
         line = line.decode('utf-8')
         line = line.strip()
@@ -136,15 +131,17 @@ def get_fqdn(ip):
         temp = line.split(':', 1)
         if temp[0].strip() == 'Name':
             result = temp[1].strip()
-    #print(result)
+    # print(result)
     return result
 
 
 '''Given a fqdn get the ipv6 address'''
 
+
 def get_ip(fqdn):
     result = 'Fail'
-    ip_nslookup_response = subprocess.Popen(["nslookup", fqdn], stdout=subprocess.PIPE)
+    ip_nslookup_response = subprocess.Popen(
+        ["nslookup", fqdn], stdout=subprocess.PIPE)
     for line in ip_nslookup_response.stdout:
         line = line.decode('utf-8')
         line = line.strip()
@@ -153,10 +150,12 @@ def get_ip(fqdn):
         temp = line.split(':', 1)
         if temp[0].strip() == 'Addresses':
             result = temp[1].strip()
-    #print(result)
+    # print(result)
     return result
 
+
 '''function returns 1 for ipv4, 2 for ipv6, and 3 for fqdn'''
+
 
 def what_is(user):
     if ':' in user:
